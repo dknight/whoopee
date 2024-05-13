68dcbe9e05919955146f8a3814b8d39dbfc1efc6
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-05-13 23:22:23 +0300

    fix: meta tags

--- layout/head.sh
+++ layout/head.sh
@@ -19,11 +19,12 @@ cat << __HEAD__
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="$description">
 <meta name="keywords" content="lua, programming, algorithms, data-structures, gamedev, game development, blog, personal">
-<meta name="author" conten="Dmitri Smirnov">
+<meta name="author" content="Dmitri Smirnov">
 <meta property="og:type" content="website">
 <meta property="og:url" content="$BLOG_HOST$URL_PREFIX$POST_URL">
 <meta property="og:image" content="$BLOG_HOST/assets/img/lua-language.gif">
-<meta http-equiv="Content-Security-Policy" content="script-src 'self', object-src 'none'">
+<meta http-equiv="Content-Security-Policy" content="script-src 'self'">
+<meta http-equiv="Content-Security-Policy" content="object-src 'none'">
 <title>$title</title>
 <link rel="icon" type="image/x-icon" href="/favicon.ico">
 <link rel="stylesheet" href="/assets/css/styles.min.css?$TS">
