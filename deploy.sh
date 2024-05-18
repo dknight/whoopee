1d2d2aadf10c4f43e1fa31c8872167dddd651af0
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-05-19 01:20:16 +0300

    fix: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+status=0
 rm -y .bloglock
 DEPLOY=1 jenny
 echo "[rclone] Uploading..."
 rclone sync .dist ftp.whoop.ee:/public_html
-echo "[rclone] OK"
+if [[ $status -eq 0 ]]; then
+	echo "[rclone] OK"
+else
+	echo "[rclone] Error"
+fi
