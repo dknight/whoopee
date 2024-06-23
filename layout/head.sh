7dbfccd6c217e6f2400d341a18a64e9c7dced73e
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-06-24 02:56:42 +0300

    fix: canonical url

--- layout/head.sh
+++ layout/head.sh
@@ -16,6 +16,11 @@ if [[ -z "$description" ]]; then
   description=$(get_default_description)
 fi
 
+canonical="$BLOG_HOST$POST_URL"
+if [[ -n "$TAGNAME" ]]; then
+  canonical="$BLOG_HOST/tag/$TAGNAME"
+fi
+
 cat << __HEAD__
 <head>
 <meta charset="utf-8">
@@ -24,7 +29,7 @@ cat << __HEAD__
 <meta name="keywords" content="lua, programming, algorithms, data-structures, gamedev, game development, blog, personal">
 <meta name="author" content="Dmitri Smirnov">
 <meta property="og:type" content="website">
-<meta property="og:url" content="$BLOG_HOST$URL_PREFIX$POST_URL">
+<meta property="og:url" content="$canonical">
 <meta property="og:image" content="$BLOG_HOST/assets/img/lua-language.gif">
 <meta http-equiv="Content-Security-Policy" content="script-src 'self'">
 <meta http-equiv="Content-Security-Policy" content="object-src 'none'">
@@ -32,6 +37,6 @@ cat << __HEAD__
 <link rel="icon" type="image/x-icon" href="/favicon.ico">
 <link rel="stylesheet" href="/assets/css/styles.min.css">
 <link rel="alternate" type="application/rss+xml" title="RSS Feed" href="/feed.xml">
-<link rel="canonical" href="$BLOG_HOST$URL_PREFIX$POST_URL">
+<link rel="canonical" href="$canonical">
 </head>
 __HEAD__
